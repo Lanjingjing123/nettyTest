b681197ab5122c65c82cb9f554ced83cd60390d8
ljj <1792539263@qq.com>
2019-07-23 18:24:45 +0800

    新增netty 客户端

---
+++ src/main/java/com/csii/ljj/netty/client/Client.java
@@ -0,0 +1,46 @@
+package com.csii.ljj.netty.client;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.*;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+import org.jboss.netty.handler.codec.string.StringDecoder;
+import org.jboss.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Client {
+    public static void main(String[] args) {
+        ClientBootstrap clientBootstrap = new ClientBootstrap();
+        // 线程池
+        ExecutorService boss = Executors.newCachedThreadPool();
+        ExecutorService worker = Executors.newCachedThreadPool();
+
+        // socket 工厂
+        clientBootstrap.setFactory(new NioClientSocketChannelFactory(boss,worker));
+
+        // 管道工厂
+        clientBootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            @Override
+            public ChannelPipeline getPipeline() throws Exception {
+                ChannelPipeline pipeline = Channels.pipeline();
+                pipeline.addLast("decoder",new StringDecoder());
+                pipeline.addLast("encoder",new StringEncoder());
+                pipeline.addLast("hiHandler",new HiHandler());
+                return pipeline;
+            }
+        });
+
+        // 连接服务器
+        ChannelFuture connect = clientBootstrap.connect(new InetSocketAddress("127.0.0.1", 10101));
+        Channel channel = connect.getChannel();
+        System.out.println("start...");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Input a String Character,please! ");
+            channel.write(scanner.next());
+        }
+    }
+}
